9237f590f3bc82b841ba6e7c4df946f21dff0045
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-20 18:32:18 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# h
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,57 @@
+;(function () {
+
+function h() {
+  var args = [].slice.call(arguments), e = null
+  function item (l) {
+    
+    function parseClass (string) {
+      var m = string.split(/([\.#]?[a-zA-Z0-9_-]+)/)
+      m.forEach(function (v) {
+        var s = v.substring(1,v.length)
+        if(!v) return 
+        if(!e)
+          e = document.createElement(v)
+        else if (v[0] === '.')
+          e.classList.add(s)
+        else if (v[0] === '#')
+          e.setAttribute('id', s)
+        
+      })
+    }
+
+    if(l == null)
+      ;
+    else if('string' === typeof l) {
+      if(!e)
+        parseClass(l)
+      else
+        e.appendChild(document.createTextNode(l))
+    }
+    else if (Array.isArray(l))
+      l.forEach(item)
+    else if(l instanceof HTMLElement)
+      e.appendChild(l)
+    else if ('object' === typeof l) {
+      for (var k in l) {
+        if('function' === typeof l[k])
+          e.addEventListener(k, l[k])
+        else if(k === 'style') {
+          for (var s in l[k])
+            e.style.setProperty(s, l[k][s])
+        }
+        else
+          e.setAttribute(k, l[k])
+      }
+    }
+  }
+  while(args.length) {
+    item(args.shift())
+  }
+  return e
+}
+
+if(typeof module === 'object')
+  module.exports = h
+else
+  this.h = h
+})()
