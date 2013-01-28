930b5feef4df822f44e59b9a29451b2ecc229c6b
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-28 20:19:47 +1300

    support observables that return Nodes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function h() {
   var args = [].slice.call(arguments), e = null
   function item (l) {
-    
+    var r
     function parseClass (string) {
       var m = string.split(/([\.#]?[a-zA-Z0-9_-]+)/)
       m.forEach(function (v) {
@@ -15,7 +15,6 @@ function h() {
           e.classList.add(s)
         else if (v[0] === '#')
           e.setAttribute('id', s)
-        
       })
     }
 
@@ -25,39 +24,70 @@ function h() {
       if(!e)
         parseClass(l)
       else
-        e.appendChild(document.createTextNode(l))
+        e.appendChild(r = document.createTextNode(l))
     }
     else if('number' === typeof l 
       || 'boolean' === typeof l
       || l instanceof Date 
       || l instanceof RegExp ) {
-        e.appendChild(document.createTextNode(l.toString()))
+        e.appendChild(r = document.createTextNode(l.toString()))
     }
+    //there might be a better way to handle this...
     else if (Array.isArray(l))
       l.forEach(item)
     else if(l instanceof HTMLElement)
-      e.appendChild(l)
+      e.appendChild(r = l)
+    else if(l instanceof Text)
+      e.appendChild(r = l)
     else if ('object' === typeof l) {
       for (var k in l) {
-        if('function' === typeof l[k])
-          e.addEventListener(k, l[k])
-        else if(k === 'style') {
-          for (var s in l[k])
-            e.style.setProperty(s, l[k][s])
+        if('function' === typeof l[k]) {
+          if(/^on\w+/.test(k)) {
+            e.addEventListener(k, l[k])
+          } else {
+            e[k] = l[k]()
+            l[k](function (v) {
+              e[k] = v
+            })
+          }
         }
-        else
-          e.setAttribute(k, l[k])
+        else if(k === 'style') {
+          for (var s in l[k]) (function(s, v) {
+            if('function' === typeof v) {
+              e.style.setProperty(s, v())
+              v(function (val) {
+                e.style.setProperty(s, val)
+              })
+            } else
+              e.style.setProperty(s, l[k][s])
+          })(s, l[k][s])
+        } else
+          e[k] = l[k]
       }
+    } else if ('function' === typeof l) {
+      //assume it's an observable!
+      var v = l()
+      e.appendChild(r = v instanceof Node ? v : document.createTextNode(v))
+
+      l(function (v) {
+        if(v instanceof Node && r.parentElement)
+          r.parentElement.replaceChild(v, r), r = v
+        else
+          r.textContent = v
+      })
+      
     }
+
+    return r
   }
-  while(args.length) {
+  while(args.length)
     item(args.shift())
-  }
+
   return e
 }
 
 if(typeof module === 'object')
   module.exports = h
 else
-  this.h = h
+  this.hyperscript = h
 })()
